a936025a08d99e8bf4b96a1ca8918e79e9566881
baubrey <baubrey@princeton.edu>
2023-10-05 03:16:07 -0400

    added rename_files for naming convention

--- subject_prep.py
+++ subject_prep.py
@@ -8,6 +8,7 @@ Typical usage example:
     patient_prep.py --sid sub-001
 """
 
+import time
 from utils import arg_parse
 from subject import Subject
 
@@ -23,8 +24,28 @@ def main():
 
     if not subject_n.base_path.exists():
         subject_n.create_dir()
-    subject_n.transfer_files()
-    subject_n.rename_files()
+    subject_n.transfer_files(["ecog"])
+
+    subject_n.edf_list()
+    # TODO: naming
+    for part, file in enumerate(sorted(subject_n.edf_files)):
+        subject_n.rename_files(file.parents[1], str(part + 1).zfill(3), file, "ecog-raw", "EDF")
+        # At this point, the directory should be empty and can be removed
+        while file.exists():
+            time.sleep(1)
+        file.parents[0].rmdir()
+
+    # NOTE: We can get the correct naming when we run downsampling/deid on nyu server
+    subject_n.audio_list()
+    for part, file in enumerate(sorted(subject_n.audio_512_files)):
+        subject_n.rename_files(file.parents[1], str(part + 1).zfill(3), file, "audio-512Hz", "wav")
+        while file.exists():
+            time.sleep(1)
+        file.parents[0].rmdir()
+
+    for part, file in enumerate(sorted(subject_n.audio_deid_files)):
+        subject_n.rename_files(file.parents[0], str(part + 1).zfill(3), file, "audio-deid", "wav")
+        # At this point, the directory should be empty and can be removed
     # TODO: move this to end
     # if not (subject_n.basePath / sid + '-summary.json').exists(): subject_n.create_summary()
 
