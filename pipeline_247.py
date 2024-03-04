d45d573d99de748ae9391389b021d8aaf7e3af85
Bobbi Aubrey <baubrey1214@gmail.com>
2024-03-04 15:46:05 -0500

    Cleaning up pipeline

--- pipeline_247.py
+++ pipeline_247.py
@@ -10,6 +10,15 @@ from classes.audio import Audio
 from classes.silence import Silence
 from classes.transcript import Transcript
 
+def get_silence_times(subject_n, file):
+
+    silence_file = subject_n.rename_files(file, "silence")
+    silence_times = Silence(silence_file)
+    silence_times.read_silence()
+    silence_times.calc_silence()
+
+    return silence_times
+
 
 def subject_prep(subject_n: Subject):
     """Set-up for processing new subject"""
@@ -41,36 +50,71 @@ def ecog_prep(subject_n: Subject):
     for file in subject_n.edf_files:
         ecog_file = Ecog(subject_n.sid, file)
 
-    ecog_file.read_EDFHeader()
-    ecog_file.end_datetime()
-    ecog_file.read_channels(10, 10000)
+        ecog_file.read_EDFHeader()
+        ecog_file.end_datetime()
+        ecog_file.read_channels(10, 10000)
 
-    # TODO: Figure out how we're splitting files
-    ecog_file.process_ecog()
-    ecog_file.name = subject_n.rename_files(ecog_file.name, "ecog-processed")
-    ecog_file.write_edf()
+        # TODO: Figure out how we're splitting files
+        ecog_file.process_ecog()
+        ecog_file.name = subject_n.rename_files(ecog_file.name, "ecog-processed")
+        ecog_file.write_edf()
 
 
-def audio_prep(transcribe_audio: Audio, silence_times: Silence):
+def audio_prep(subject_n: Subject):
     """Prepare audio file for transcription"""
-    transcribe_audio.read_audio()
 
-    transcribe_audio.crop_audio(silence_times)
-    transcribe_audio.slow_audio()
-    # audio.denoise_audio(transcribe_audio)
-    transcribe_audio.write_audio()
+    for file in subject_n.audio_deid_files:
+        transcribe_audio = Audio(subject_n.sid, file)
+
+        silence_times = get_silence_times(subject_n, file)
+    
+        transcribe_audio.read_audio()
+
+        transcribe_audio.crop_audio(silence_times)
+        transcribe_audio.slow_audio()
+        # audio.denoise_audio(transcribe_audio)
+        transcribe_audio.out_name = subject_n.rename_files(transcribe_audio.name, "audio-transcribe")
+        transcribe_audio.write_audio()
 
 
-def transcript_prep(transcript_file: Transcript, silence_times: Silence):
+def transcript_prep(subject_n: Subject, silence_times: Silence):
     """Prepare subject-level transcript"""
-    transcript_file.parse_xml()
-    transcript_file.convert_timedelta()
-    transcript_file.compress_transcript(0.05)
 
-    onset_day, onset_time = transcript_file.get_audio_info_csv()
-    transcript_file.add_dt(onset_day, onset_time)
+    for file in subject_n.audio_deid_files:
+        transcript_filename = subject_n.rename_files(file, "transcript")
+        # TODO: decide what to do about possible file mismatches (if we have an audio file but no transcript, etc.)
+        if transcript_filename.is_file():
+            transcript_file = Transcript(subject_n.sid, transcript_filename)
 
-    transcript_file.agg_silences(silence_times)
+            if "silence_times" not in locals():
+                 silence_times = get_silence_times(subject_n, file)
+
+            transcript_prep(transcript_file, silence_times)
+
+        subject_n.transcript = pd.concat(
+            [subject_n.transcript, transcript_file.transcript]
+        )
+        
+        transcript_file.parse_xml()
+        transcript_file.convert_timedelta()
+        transcript_file.compress_transcript(0.05)
+
+        onset_day, onset_time = transcript_file.get_audio_info_csv()
+        transcript_file.add_dt(onset_day, onset_time)
+
+        transcript_file.agg_silences(silence_times)
+    
+    # Write subject-level transcript
+    subject_n.transcript = (
+        subject_n.transcript.rename_axis("part_idx")
+        .sort_values(by=["onset", "part_idx"])
+        .reset_index()
+    )
+
+    subject_n.transcript.to_csv(
+        subject_n.filenames["transcript"].parents[1]
+        / "_".join([subject_n.sid, "transcript.csv"])
+    )
 
 
 def main():
@@ -98,6 +142,7 @@ def main():
 
     for fun in fun_list:
         if fun.__name__ in steps:
+            print(fun.__name__ )
             fun(subject_n)
 
     breakpoint()
@@ -109,65 +154,6 @@ def main():
     )
 
     config.write_config_old()
-
-    # Loop over files for processing
-    for file in subject_n.edf_files:
-        # Step_003: ecog_prep: Split EDF files, ECoG signal processing
-        if "3" in steps:
-            # TODO: IMPORTANT Find matching EDF file
-            ecog_file = Ecog(args, file)
-            ecog_prep(ecog_file)
-        else:
-            print("Skipping step_003: ecog_prep")
-        # Step_004: audio_prep: Prepare audio file for transcription
-        if "4" in steps:
-            transcribe_audio = Audio(subject_n.sid, file)
-
-            [_, part, _] = transcribe_audio.in_name.stem.split("_")
-            transcribe_audio.out_name = subject_n.rename_files(
-                file, part, "audio_transcribe"
-            )
-
-            silence_file = subject_n.rename_files(file, part, "silence")
-            silence_times = Silence(silence_file)
-            silence_times.read_silence()
-            silence_times.calc_silence()
-
-            audio_prep(transcribe_audio, silence_times)
-        else:
-            print("Skipping step_004: audio_prep")
-        if "5" in steps:
-            [_, part, _] = file.stem.split("_")
-            transcript_filename = subject_n.rename_files(file, part, "transcript")
-            # TODO: decide what to do about possible file mismatches (if we have an audio file but no transcript, etc.)
-            if transcript_filename.is_file():
-                transcript_file = Transcript(subject_n.sid, transcript_filename)
-
-                if "silence_times" not in locals():
-                    silence_file = subject_n.rename_files(file, part, "silence")
-                    silence_times = Silence(silence_file)
-                    silence_times.read_silence()
-                    silence_times.calc_silence()
-
-                transcript_prep(transcript_file, silence_times)
-
-            subject_n.transcript = pd.concat(
-                [subject_n.transcript, transcript_file.transcript]
-            )
-        else:
-            print("Skipping step_005: transcript_prep")
-
-    # Write subject-level transcript
-    subject_n.transcript = (
-        subject_n.transcript.rename_axis("part_idx")
-        .sort_values(by=["onset", "part_idx"])
-        .reset_index()
-    )
-
-    subject_n.transcript.to_csv(
-        subject_n.filenames["transcript"].parents[1]
-        / "_".join([subject_n.sid, "transcript.csv"])
-    )
 
     # TODO: move this to end
     # if not (subject_n.basePath / sid + '-summary.json').exists(): subject_n.create_summary()
